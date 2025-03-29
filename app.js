1c513ff6c1da9b4e1d93653d97bc7b8df8a90c74
Your Name <s-sara.ali@zewailcity.edu.eg>
2025-03-29 23:35:31 +0200

    Added delete button feature

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const path = require('path');
 const connectToDatabase = require('./config/database');
 const itemRoutes = require('./routes/itemRoutes');
 
@@ -9,10 +10,24 @@ connectToDatabase();
 const app = express();
 const port = process.env.PORT || 3000;
 
+
 app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+
+
 app.use(express.urlencoded({ extended: true }));
 
-app.use(express.static('public'));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+
 app.use('/', itemRoutes);
 
-app.listen(port, () => console.log(`Listening on http://localhost:${port}`));
+
+app.use((err, req, res, next) => {
+    console.error(" Error:", err.message);
+    res.status(500).send("Internal Server Error");
+});
+
+
+app.listen(port, () => console.log(` Server is running on http://localhost:${port}`));
--- controllers/itemController.js
+++ controllers/itemController.js
@@ -3,7 +3,7 @@ const Item = require('../models/item');
 exports.getItems = async (req, res) => {
   try {
     const items = await Item.find();
-    res.render('index', { items });
+    res.render('index', { items }); 
   } catch (err) {
     res.status(500).send('Error fetching items');
   }
@@ -17,5 +17,16 @@ exports.addItem = async (req, res) => {
     res.redirect('/');
   } catch (err) {
     res.status(500).send('Error saving item');
+  }
+};
+
+ 
+exports.deleteItem = async (req, res) => {
+  try {
+    await Item.findByIdAndDelete(req.params.id);
+    res.redirect('/'); 
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Error deleting item');
   }
 };
